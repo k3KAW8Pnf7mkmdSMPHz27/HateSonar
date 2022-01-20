d8cacc1fb020f027b1ba19cf397a3b6439818d1b
Jonatan Asketorp <2598631+k3KAW8Pnf7mkmdSMPHz27@users.noreply.github.com>
2022-01-20 18:47:19 -0500

    Fix codestyle

--- hatesonar/api.py
+++ hatesonar/api.py
@@ -13,26 +13,25 @@ import onnxruntime as rt
 
 
 class Sonar(object):
-    _map = {0: 'hate_speech', 1: 'offensive_language', 2: 'neither'}
+    _map = {0: "hate_speech", 1: "offensive_language", 2: "neither"}
 
     def __init__(self):
-        base_dir = os.path.join(os.path.dirname(__file__), './data')
-        pipeline_file = os.path.join(base_dir, 'pipeline.onnx')
+        base_dir = os.path.join(os.path.dirname(__file__), "./data")
+        pipeline_file = os.path.join(base_dir, "pipeline.onnx")
         self.pipeline = rt.InferenceSession(pipeline_file)
 
     def ping(self, text: str) -> dict:
         assert isinstance(text, str)
 
-        proba = self.pipeline.run(None, {'input': [text]})[1][0]
+        proba = self.pipeline.run(None, {"input": [text]})[1][0]
 
         res = {
-            'text': text,
-            'top_class': Sonar._map[np.argmax(proba)],
-            'classes': [
-                {'class_name': Sonar._map[k],
-                 'confidence': proba[k]}
+            "text": text,
+            "top_class": Sonar._map[np.argmax(proba)],
+            "classes": [
+                {"class_name": Sonar._map[k], "confidence": proba[k]}
                 for k in sorted(Sonar._map)
-            ]
+            ],
         }
 
         return res
