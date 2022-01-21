e7cb20109febf07a05a8d0a5d713ebae42e5e7b7
Jonatan Asketorp <2598631+k3KAW8Pnf7mkmdSMPHz27@users.noreply.github.com>
2022-01-20 19:17:23 -0500

    Bump setup.py

--- setup.py
+++ setup.py
@@ -7,48 +7,44 @@ import sys
 from setuptools import find_packages, setup
 
 # Package meta-data.
-NAME = 'hatesonar'
-DESCRIPTION = 'Hate Speech Detection Library for Python'
-URL = 'https://github.com/Hironsan/HateSonar'
-EMAIL = 'hiroki.nakayama.py@gmail.com'
-AUTHOR = 'Hironsan'
-LICENSE = 'MIT'
+NAME = "hatesonar-onnx"
+DESCRIPTION = "Fork of Hironsan's Hate Speech Detection Library, HateSonar, using onnx."
+URL = "https://github.com/k3KAW8Pnf7mkmdSMPHz27/HateSonar"
+EMAIL = "jonatan.asketorp@gmail.com"
+AUTHOR = "Jonatan Asketorp"
+LICENSE = "MIT"
 
 here = os.path.abspath(os.path.dirname(__file__))
-with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
-
-if sys.argv[-1] == 'publish':
-    os.system('python setup.py sdist bdist_wheel')
-    os.system('twine upload dist/*')
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist bdist_wheel")
+    os.system("twine upload dist/*")
     sys.exit()
 
-required = [
-    'numpy>=1.14.0', 'pandas>=0.22.0', 'scikit-learn>=0.19.1', 'scipy>=1.0.0', 'joblib>=0.16.0'
-]
+required = ["numpy>=1.14.0", "onnxruntime>=1.10.0"]
 
 setup(
     name=NAME,
-    version='0.0.7',
+    version="0.0.8",
     description=DESCRIPTION,
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     author=AUTHOR,
     author_email=EMAIL,
     url=URL,
-    packages=find_packages(exclude=('tests',)),
+    packages=find_packages(exclude=("tests",)),
     install_requires=required,
     include_package_data=True,
     license=LICENSE,
     classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy'
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
     ],
 )
