416ec939001adfb39f367ce7cfeb0e52fa25d425
Jonatan Asketorp <2598631+k3KAW8Pnf7mkmdSMPHz27@users.noreply.github.com>
2023-09-21 10:27:26 -0400

    Fix onnxruntime version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ if sys.argv[-1] == "publish":
     os.system("twine upload dist/*")
     sys.exit()
 
-required = ["numpy>=1.16.0", "onnxruntime>=1.9.0"]
+required = ["numpy>=1.16.0", "onnxruntime>=1.9.0, 1.16.0"]
 
 setup(
     name=NAME,
