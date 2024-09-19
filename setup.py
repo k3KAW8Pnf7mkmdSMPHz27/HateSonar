88136a7c5b3e58f39ba7f1025e745f5dffc06da4
Jonatan Asketorp <2598631+k3KAW8Pnf7mkmdSMPHz27@users.noreply.github.com>
2024-09-19 17:02:14 -0500

    Unused likely incorrect classifiers

--- setup.py
+++ setup.py
@@ -42,10 +42,6 @@ setup(
     license=LICENSE,
     classifiers=[
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
 )
